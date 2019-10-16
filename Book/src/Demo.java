7241ead5a304dd9335747a5543f56847518c2a6d
Coalesce <Coalesce@DESKTOP-F7TUI75>
2019-10-16 17:29:00 +0530

    Commit

---
+++ Book/src/Demo.java
@@ -0,0 +1,12 @@
+
+public class Demo {
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		int a=10;
+		for(int i=0;i<10;i++){
+			System.out.println("hello java");
+			
+		}
+	}
+
+}
